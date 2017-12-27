ade7f85aad1be7c1a64e6030c1b4857113213832
hignz <higz-14@live.com>
2017-12-27 17:44:49 +0000

    More cleanup
    Code cleanup, removed some colliders

--- Assets/Scripts/exitHealthCentre.cs
+++ Assets/Scripts/exitHealthCentre.cs
@@ -1,30 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public class ExitHealthCentre : MonoBehaviour {
 
-public class exitHealthCentre : MonoBehaviour {
-
-    [SerializeField] private string LoadNextlevel;
-
-  
-    
+    [SerializeField]
+    private string LoadNextlevel;
+ 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            PlayerController Player = collision.gameObject.GetComponent<PlayerController>();
+            Player Player = collision.gameObject.GetComponent<Player>();
             SceneManager.LoadScene(LoadNextlevel);
             Invoke("PlayerPositionAfterExit", 1f);
-            
-         
         }
         
     }
     void PlayerPositionAfterExit()
     {
-        PlayerController Player = gameObject.GetComponent<PlayerController>();
+        Player Player = gameObject.GetComponent<Player>();
         Player.transform.position = new Vector2(5.83f, -0.80f);
     }
 }
