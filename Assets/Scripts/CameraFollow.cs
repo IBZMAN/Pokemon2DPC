e24f61133f14d1175db4e082dd942b0a20be4e5e
hignz <higz-14@live.com>
2017-12-27 16:43:03 +0000

    Added new camerafollow and experimental shadow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool restrictCamera = false;
+
+    [SerializeField]
+    private float xMin;
+
+    [SerializeField]
+    private float yMin;
+
+    [SerializeField]
+    private float xMax;
+
+    [SerializeField]
+    private float yMax;
+
+    [SerializeField]
+    private Transform target;
+
+    void LateUpdate()
+    {
+        if (restrictCamera)
+        {
+            transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Shadow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+
+    public Vector2 offset = new Vector2(-3, -3);
+
+    private SpriteRenderer sprCaster;
+    private SpriteRenderer sprShadow;
+
+    private Transform transCaster;
+    private Transform transShadow;
+
+    public Material shadowMaterial;
+    public Color shadowColor;
+
+    // Use this for initialization
+    void Start () {
+        transCaster = transform;
+        transShadow = new GameObject().transform;
+        transShadow.parent = transCaster;
+        transShadow.gameObject.name = "shadow";
+        transShadow.localRotation = Quaternion.identity;
+
+        sprCaster = GetComponent<SpriteRenderer>();
+        sprShadow = transShadow.gameObject.AddComponent<SpriteRenderer>();
+
+        sprShadow.material = shadowMaterial;
+        sprShadow.color = shadowColor;
+
+        sprShadow.sortingLayerName = sprCaster.sortingLayerName;
+        sprShadow.sortingOrder = sprCaster.sortingOrder - 1;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transShadow.position = new Vector2(transCaster.position.x + offset.x, transCaster.position.y + offset.y);
+
+        sprShadow.sprite = sprCaster.sprite;
+	}
+}
