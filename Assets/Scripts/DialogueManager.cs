e5cf7824906cd3726653750386277cb7e38ee099
hignz <higz-14@live.com>
2017-12-28 19:20:31 +0000

    New pause menu, import textmesh pro, pause menu bug fix

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogText;
 
     private Queue<string> sentences;
 
@@ -14,5 +19,33 @@ public class DialogueManager : MonoBehaviour {
     public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogText.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of conversation");
     }
 }
