2b2c33912630cdaaf249d0524d662c65926834be
IBZMAN <34863485+IBZMAN@users.noreply.github.com>
2017-12-30 21:40:04 +0000

    added tile mapper

---
+++ Assets/Resources/2D Tile Map Creator/Scripts/EditorHitbox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorHitbox : MonoBehaviour 
+{
+	void Start () 
+	{
+	    if(Application.isPlaying == true)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
