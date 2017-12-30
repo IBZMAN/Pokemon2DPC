23c034c4dd347c2c0257723ee8d00c1de983b929
IBZMAN <34863485+IBZMAN@users.noreply.github.com>
2017-12-30 15:36:38 +0000

    Added in Truck/Anim

---
+++ Assets/SceneTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour {
+
+    public int sceneTriggerAmnt = 1;
+    public float elapsedTime = 0;
+    public float timeUntilTrigger = 7;
+    public GameObject sceneTrigger;
+    Vector2 sceneTriggerPosition = new Vector2(-76.8f, -30.38f);
+    public int i = 0;
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > timeUntilTrigger)
+        {
+            while (i < sceneTriggerAmnt)
+            {
+                GameObject newSceneTrigger = Instantiate(sceneTrigger);
+                newSceneTrigger.transform.position = sceneTriggerPosition;
+                i++;
+            }
+        }
+    }
+   
+}
---
+++ Assets/Scripts/FuccsvillSceneTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FuccsvillSceneTrigger : MonoBehaviour {
+
+    [SerializeField] private string NextSceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(NextSceneToLoad);
+        }
+    }
+}
